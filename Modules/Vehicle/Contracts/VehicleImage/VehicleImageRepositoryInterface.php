785442dea0a21a7dcb3389af593d78f162bd4009
Ibrahim <ibrahim@Ibrahims-MacBook-Air.local>
2026-07-15 14:21:59 +0300

    feature/vehicle: Add VehicleImage contracts

---
+++ Modules/Vehicle/Contracts/VehicleImage/VehicleImageRepositoryInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Vehicle\Contracts\VehicleImage;
+
+use App\Support\Contracts\CrudRepositoryInterface;
+
+interface VehicleImageRepositoryInterface extends CrudRepositoryInterface {}
