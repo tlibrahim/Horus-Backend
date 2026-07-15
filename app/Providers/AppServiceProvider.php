db8bb373f09eb1f9ce603c552c502e09b82c4593
Ibrahim <ibrahim@Ibrahims-MacBook-Air.local>
2026-07-15 17:32:50 +0300

    Added global file storage

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Support\Contracts\FileStorageServiceInterface;
+use App\Support\Services\FileStorageService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton(
+            FileStorageServiceInterface::class,
+            FileStorageService::class,
+        );
     }
 
     /**
